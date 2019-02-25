0458bf9fa1c28d19b8b70e74fad94c2e9140b07f
ming <apiming0369@gmail.com>
2019-02-25 21:53:22 +0800

    Add IntervalSplitsByNumber, IntervalSplitBySize

--- util/util.go
+++ util/util.go
@@ -1,6 +1,20 @@
 package util
 
-func IntervalSplits(begin, end, split int64) [][2]int64 {
+func IntervalSplitsByNumber(begin, end, split int64) [][2]int64 {
+	result := [][2]int64{}
+
+	for i := begin; i <= end; i += split {
+		b := i + split - 1
+		if b > end {
+			b = end
+		}
+		result = append(result, [2]int64{i, b})
+	}
+
+	return result
+}
+
+func IntervalSplitBySize(begin, end, split int64) [][2]int64 {
 	result := [][2]int64{}
 
 	c := (end - begin) / split
--- util/util_test.go
+++ util/util_test.go
@@ -2,10 +2,14 @@ package util
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestIntervalSplits(t *testing.T) {
+	//fmt.Println(IntervalSplits(1550419200, 1551023999, 60))
+}
 
+func TestIntervalSplitsByNumber(t *testing.T) {
+	//
 }
